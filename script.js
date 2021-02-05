5c9f6cc8db5eb81ddb838f66cff7d15f6f0d36ba
Najmul Huda <najmul.myself@gmail.com>
2021-02-06 03:00:54 +0600

    all total ened

--- script.js
+++ script.js
@@ -9,4 +9,23 @@ function eventHandler(whichClass,isIncrese){
         newValue = parseValue - 1 ;
     }
     inputValue.value = newValue;
+    
+    totalCount();
+}
+
+function totalCount(){
+    const firstClassCount = getInput('first')
+    const economyClassCount = getInput('economy')
+    const subTotal = firstClassCount * 150 + economyClassCount * 100 ;
+    const taxCount = subTotal * 0.1;
+    document.getElementById('sub-total').innerText = '$' + subTotal;
+    document.getElementById('tax-count').innerText = '$' + taxCount;
+    const grandTotal = subTotal + taxCount;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
+}
+
+function getInput(whichClass){
+    const ticketQuantity = document.getElementById(whichClass + '-input');
+    const parseQuantity = parseInt(ticketQuantity.value);
+    return parseQuantity;
 }
