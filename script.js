2d196448a6c1fe2689644c74c22c7765955df44b
Najmul Huda <najmul.myself@gmail.com>
2021-02-07 00:36:52 +0600

    Finishing Project

--- script.js
+++ script.js
@@ -9,19 +9,29 @@ function eventHandler(whichClass,isIncrese){
         newValue = parseValue - 1 ;
     }
     inputValue.value = newValue;
+
     
     totalCount();
 }
 
 function totalCount(){
-    const firstClassCount = getInput('first')
-    const economyClassCount = getInput('economy')
+    const firstClassCount = getInput('first');
+    const economyClassCount = getInput('economy');
+    // Booking Information Update
+    document.getElementById('first-count').innerText = firstClassCount;
+    document.getElementById('economy-count').innerText = economyClassCount;
+    document.getElementById('amountOfFirst').innerText = firstClassCount * 150;
+    document.getElementById('amountOfEconomy').innerText = economyClassCount * 100;
+
     const subTotal = firstClassCount * 150 + economyClassCount * 100 ;
     const taxCount = subTotal * 0.1;
     document.getElementById('sub-total').innerText = '$' + subTotal;
     document.getElementById('tax-count').innerText = '$' + taxCount;
     const grandTotal = subTotal + taxCount;
     document.getElementById('grand-total').innerText = '$' + grandTotal;
+
+    document.getElementById('booking-total-With-VAT').innerText = '$' + grandTotal;
+    document.getElementById('sub-booking-total').innerText = '$' + grandTotal;
 }
 
 function getInput(whichClass){
@@ -29,3 +39,10 @@ function getInput(whichClass){
     const parseQuantity = parseInt(ticketQuantity.value);
     return parseQuantity;
 }
+
+document.getElementById('book-now').addEventListener('click',function(){
+    const popUp = document.querySelector('.confirmation-box').style.display = 'block';
+    document.getElementById('back-home').addEventListener('click',function(){
+        const backHome = document.querySelector('.confirmation-box').style.display = 'none';
+    })
+})
